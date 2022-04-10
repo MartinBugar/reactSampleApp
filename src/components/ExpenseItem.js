edfb1fd9e842187ad045c7a2520e59e67e484570
martyx <martin1.bugar@gmail.com>
2022-04-10 13:11:23 +0200

    props childer and composition

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import '../components/ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
 
@@ -8,13 +9,13 @@ function ExpenseItem(props) {
     // const expenseAmount = 294;
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <div className="expense-item__description">
                 <ExpenseDate date={props.date}/>
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
