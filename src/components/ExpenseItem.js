b8f737ffbfcc692a08206119f75f6a3a0bd33238
martyx <martin1.bugar@gmail.com>
2022-04-10 12:27:17 +0200

    first commit of this react app

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import '../components/ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
+
+    // const expenseDate = new Date(2021, 2, 28);
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 294;
+
+    return (
+        <div className="expense-item">
+            <div className="expense-item__description">
+                <ExpenseDate date={props.date}/>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
